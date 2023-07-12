666f24c4c72d875456946cb3a4ed81e37b50df39
Richard Faulkner <132923452+Condone8381@users.noreply.github.com>
2023-07-12 14:50:23 -0400

    Added DC-Variables

--- Omaha_Demo/vm-dc2-variables.tf
+++ Omaha_Demo/vm-dc2-variables.tf
@@ -1,10 +1,10 @@
 ########################################
-## AD DC1 Virtual Machine - Variables ##
+## AD DC2 Virtual Machine - Variables ##
 ########################################
 
 # Azure virtual machine settings #
 
-variable "dc1_vm_size" {
+variable "dc2_vm_size" {
   type        = string
   description = "Size (SKU) of the virtual machine to create"
   default     = "Standard_DS2_v2"
@@ -12,13 +12,13 @@ variable "dc1_vm_size" {
 
 # Azure virtual machine storage settings #
 
-variable "dc1_delete_os_disk_on_termination" {
+variable "dc2_delete_os_disk_on_termination" {
   type        = string
   description = "Should the OS Disk (either the Managed Disk / VHD Blob) be deleted when the Virtual Machine is destroyed?"
   default     = "true"  # Update for your environment
 }
 
-variable "dc1_delete_data_disks_on_termination" {
+variable "dc2_delete_data_disks_on_termination" {
   description = "Should the Data Disks (either the Managed Disks / VHD Blobs) be deleted when the Virtual Machine is destroyed?"
   type        = string
   default     = "true" # Update for your environment
@@ -26,14 +26,14 @@ variable "dc1_delete_data_disks_on_termination" {
 
 # Active Directory Configuration #
 
-# domain controller 1 name
-variable "ad_dc1_name" {
+# domain controller 2 name
+variable "ad_dc2_name" {
   type        = string
-  description = "This variable defines the name of AD Domain Controller 1"
+  description = "This variable defines the name of AD Domain Controller 2"
 }
 
 # domain controller 1 private ip address
-variable "ad_dc1_ip_address" {
+variable "ad_dc2_ip_address" {
   type        = string
-  description = "This variable defines the private ip address of AD Domain Controller 1"
+  description = "This variable defines the private ip address of AD Domain Controller 2"
 }
