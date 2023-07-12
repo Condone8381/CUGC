7f0f281d97828c29390fedd027cb410172cf678b
Richard Faulkner <132923452+Condone8381@users.noreply.github.com>
2023-07-12 14:46:52 -0400

    Update vm-dc1-output.tf

--- Omaha_Demo/vm-dc1-output.tf
+++ Omaha_Demo/vm-dc1-output.tf
@@ -7,11 +7,6 @@ output "ad_dc1_vm_name" {
   value       = azurerm_windows_virtual_machine.dc1-vm.name
 }
 
-output "ad_dc1_vm_ip_address" {
-  description = "Domain Controller 1 IP Address"
-  value       = azurerm_public_ip.dc1-eip.ip_address
-}
-
 # output "ad_dc1_script_debug_1" {
 #  value = local.dc1_install_ad_1
 # }
