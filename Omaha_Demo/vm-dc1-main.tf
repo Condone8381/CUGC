7537e3b684deb615fc6132572af2647879f710e4
Richard Faulkner <132923452+Condone8381@users.noreply.github.com>
2023-07-12 14:58:33 -0400

    Update vm-dc1-main.tf

--- Omaha_Demo/vm-dc1-main.tf
+++ Omaha_Demo/vm-dc1-main.tf
@@ -49,8 +49,6 @@ resource "azurerm_network_security_group" "dc1-vm-nsg" {
 
 # Create a NIC for DC1
 resource "azurerm_network_interface" "dc1-nic" {
-  depends_on=[azurerm_public_ip.dc1-eip]
-
   name                    = "${var.ad_dc1_name}-nic"
   location                = azurerm_resource_group.terraform-resource-group.location
   resource_group_name     = azurerm_resource_group.terraform-resource-group.name
