4a2d7347df9a5d2cff63f084d694ff7db49265b1
Richard Faulkner <132923452+Condone8381@users.noreply.github.com>
2023-07-13 16:31:25 -0400

    Update vm-sql-main.tf

--- Omaha_Demo/vm-sql-main.tf
+++ Omaha_Demo/vm-sql-main.tf
@@ -3,12 +3,12 @@ resource "azurerm_network_interface" "terraform-sql-server-interface" {
   name                = "terraform-sql-server-interface"
   location            = var.location
   resource_group_name = azurerm_resource_group.terraform-resource-group.name
+  dns_servers             = local.dns_servers
 
   ip_configuration {
     name                          = "server"
     subnet_id                     = azurerm_subnet.terraform-server-subnet.id
     private_ip_address_allocation = "Dynamic"
-    dns_servers             = local.dns_servers
   }
 
   depends_on = [azurerm_subnet_network_security_group_association.server-subnet-association]
