76e054779f80a7f6bba967a52f15f3c0b609a7a3
Richard Faulkner <132923452+Condone8381@users.noreply.github.com>
2023-07-13 13:51:10 -0400

    Update delivery-controller-main.tf

--- Omaha_Demo/delivery-controller-main.tf
+++ Omaha_Demo/delivery-controller-main.tf
@@ -2,15 +2,9 @@
 ## Delivery Controllers - Main ##
 #################################
 
-# Locals
-locals {
-  dns_servers = [ var.ad_dc1_ip_address, var.ad_dc2_ip_address ]
-  dc_servers = [ var.ad_dc1_ip_address, var.ad_dc2_ip_address ]
-}
-
 # Create an availability set for delivery controllers
 resource "azurerm_availability_set" "ddc-availability-set" {
-  name                         = "dc-availability-set-${var.environment}"
+  name                         = "ddc-availability-set-${var.environment}"
   resource_group_name          = azurerm_resource_group.terraform-resource-group.name
   location                     = azurerm_resource_group.terraform-resource-group.location
   platform_fault_domain_count  = 3
