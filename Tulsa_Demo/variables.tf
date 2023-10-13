763faad0a7cc4e4a2ae0298a7893be39946dc10b
Richard Faulkner <richard.faulkner@citrix.com>
2023-10-13 10:35:08 -0400

    Update variables.tf

--- Tulsa_Demo/variables.tf
+++ Tulsa_Demo/variables.tf
@@ -1,5 +1,6 @@
 variable "resource_group_name" {
   description = "Name for the existing resource group ADC is deployed in."
+  default     = "cugc-demos-louisville"
 }
 
 variable "location" {
@@ -16,12 +17,12 @@ variable "management_subnet_id" {
 
 variable "ssh_public_key_file" {
   description = "Public key file for accessing the ubuntu machine."
-  default     = "~/.ssh/id_rsa.pub"
+  default     = "~/.ssh/id_ed25519.pub"
 }
 
 variable "ssh_private_key_file" {
   description = "Private key file for accessing the ubuntu machine."
-  default     = "~/.ssh/id_rsa"
+  default     = "~/.ssh/id_ed25519"
 }
 
 variable "admin_user" {
@@ -59,4 +60,33 @@ variable "adc_admin_username" {
 
 variable "adc_admin_password" {
   description = "Password for the Citrix ADC admin user. Must be sufficiently complex to pass azurerm provider checks."
+}
+
+variable "citrixadc_rpc_node_password" {
+  description = "The new ADC RPC node password that will replace the default one on both ADC instances. [Learn More about RPCNode](https://docs.citrix.com/en-us/citrix-adc/current-release/getting-started-with-citrix-adc/change-rpc-node-password.html)"
+}
+
+variable "virtual_network_address_space" {
+  description = "Address space for the virtual network."
+}
+
+variable "management_subnet_address_prefix" {
+  description = "The address prefix that will be used for the management subnet. Must be contained inside the VNet address space"
+}
+
+variable "client_subnet_address_prefix" {
+  description = "The address prefix that will be used for the client subnet. Must be contained inside the VNet address space"
+}
+
+variable "server_subnet_address_prefix" {
+  description = "The address prefix that will be used for the server subnet. Must be contained inside the VNet address space"
+}
+
+variable "controlling_subnet" {
+  description = "The CIDR block of the machines that will be allowed access to the management subnet."
+}
+
+variable "adc_vm_size" {
+  description = "Size for the ADC machine. Must allow for 3 NICs."
+  default     = "Standard_DS3_v2"
 }
