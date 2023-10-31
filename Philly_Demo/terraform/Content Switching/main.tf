6deb894cd16711b91ca752c7a0bb3b369af538bb
Richard Faulkner <richard.faulkner@citrix.com>
2023-10-31 15:10:42 -0400

    Update main.tf

--- Philly_Demo/terraform/Content Switching/main.tf
+++ Philly_Demo/terraform/Content Switching/main.tf
@@ -5,7 +5,7 @@ resource "citrixadc_nsfeature" "tf_nsfeature" {
 }
 
 resource "citrixadc_nsip" "snip" {
-  ipaddress = "10.0.1.6"
+  ipaddress = "192.168.3.10"
   type      = "SNIP"
   netmask   = "255.255.255.0"
 }
@@ -41,5 +41,44 @@ resource "citrixadc_service" "web-server-green" {
 
 resource "citrixadc_lbvserver_service_binding" "lb_binding2" {
   name        = citrixadc_lbvserver.tf_lbvserver2.name
-  servicename = citrixadc_service.web-server-red.name
+  servicename = citrixadc_service.web-server-green.name
 }
+
+resource "citrixadc_csaction" "tf_csaction1" {
+  name            = "tf_csaction1"
+  targetlbvserver = citrixadc_lbvserver.tf_lbvserver1.name
+}
+resource "citrixadc_cspolicy" "tf_policy_red" {
+  policyname = var.cspolicy1_name
+  rule       = var.cspolicy1_rule
+  action     = citrixadc_csaction.tf_csaction1.name
+}
+
+resource "citrixadc_csaction" "tf_csaction2" {
+  name            = "tf_csaction2"
+  targetlbvserver = citrixadc_lbvserver.tf_lbvserver2.name
+}
+resource "citrixadc_cspolicy" "tf_policy_green" {
+  policyname = var.cspolicy2_name
+  rule       = var.cspolicy2_rule
+  action     = citrixadc_csaction.tf_csaction2.name
+}
+
+resource "citrixadc_csvserver" "tf_csvserver" {
+  ipv46       = var.csvserver_ipv46
+  name        = var.csvserver_name
+  port        = var.csvserver_port
+  servicetype = var.csvserver_servicetype
+}
+
+resource "citrixadc_csvserver_cspolicy_binding" "tf_csvscspolbind_red" {
+  name       = citrixadc_csvserver.tf_csvserver.name
+  policyname = citrixadc_cspolicy.tf_policy_red.policyname
+  priority   = 100
+}
+resource "citrixadc_csvserver_cspolicy_binding" "tf_csvscspolbind_green" {
+  name       = citrixadc_csvserver.tf_csvserver.name
+  policyname = citrixadc_cspolicy.tf_policy_green.policyname
+  priority   = 110
+}
+
